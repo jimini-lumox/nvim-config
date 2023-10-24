4cb7b8086c49b364bc03eb8ce4282b26afe0a67b
fb <you@example.com>
2023-10-24 16:38:18 +1000

    Update telescope version

--- nvim/config/nvim/lua/plugins.lua
+++ nvim/config/nvim/lua/plugins.lua
@@ -19,7 +19,7 @@ return require('packer').startup(function(use)
     use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
     --use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 	use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'}, { 'nvim-treesitter/nvim-treesitter' } }
 	}
